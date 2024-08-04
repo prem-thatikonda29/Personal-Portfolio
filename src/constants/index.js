726a7bb7c92fd407a888223e59bce1b00bcab1ce
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-08-04 16:18:34 +0530

    added projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -229,20 +229,20 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Connexus",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Web-based platform that allows users to search for and apply to jobs, manage connections with other individuals across the world, providing a convenient solution for networking and job application needs.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
-        color: "green-text-gradient",
+        name: "firebase",
+        color: "orange-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "scss",
         color: "pink-text-gradient",
       },
     ],
@@ -250,7 +250,7 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Job IT",
+    name: "Agrosphere",
     description:
       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
@@ -271,7 +271,7 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Trip Guide",
+    name: "Coming soon",
     description:
       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
     tags: [
