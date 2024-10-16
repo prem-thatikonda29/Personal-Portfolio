fbc9209cc9791f5916880bdbf0ad6fc4dcbb5fc5
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-10-17 01:15:20 +0530

    changed hero section 3d model positions for mobile view

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,26 +3,26 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
-import Robot from "./Robot";
+// import Robot from "./Robot";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={4} groundColor="purple" />
       <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
+        position={[-20, 50, 20]}
+        angle={0.15}
         penumbra={1}
-        intensity={1}
+        intensity={2}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={1.2} />
       <primitive
-        object={robot.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        object={computer.scene}
+        scale={isMobile ? 0.4 : 0.65}
+        position={isMobile ? [-2, -1.5, -1.8] : [0, -2.5, -1.2]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -67,8 +67,8 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        {/* <Computers isMobile={isMobile} /> */}
-        <Robot />
+        <Computers isMobile={isMobile} />
+        {/* <Robot /> */}
       </Suspense>
 
       <Preload all />
